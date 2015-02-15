a03d67da30ce1c7ebad563651097e73272de23e4
MeepDarknessMeep <meepdarknessmeep@yahoo.com>
2015-02-15 13:59:36 -0500

    Update some things and fix a bug including not able to get chat panel (causing error)

--- gmodchatmod/lua/autorun/client/chat_mod.lua
+++ gmodchatmod/lua/autorun/client/chat_mod.lua
@@ -1,14 +1,16 @@
+if(CHAT_MOD) then return; end
 CHAT_HISTORY 		= "history";
 CHAT_FILTER_BUTTON	= "filters";
 CHAT_INPUT_LINE		= "input_main";
 CHAT_INPUT			= "input";
+CHAT_MAIN			= "main";
 
 CHAT_MOD = {
 	--[[ stores children in CHAT_MOD[value] (by name) ]]--
 	children = {
-		["HudChatHistory"] 		= "history";
-		["ChatFiltersButton"] 	= "filters";
-		["ChatInputLine"]		= "input_main";
+		["HudChatHistory"] 		= CHAT_HISTORY;
+		["ChatFiltersButton"] 	= CHAT_FILTER_BUTTON;
+		["ChatInputLine"]		= CHAT_INPUT_LINE;
 	};
 	panels = {};
 };
@@ -35,7 +37,7 @@ function CHAT_MOD:Get(name)
 end
 
 function CHAT_MOD:Init(chat)
-	self.chat = chat;
+	self.panels[CHAT_MAIN] = chat;
 	local children = chat:GetChildren();
 	local searching = self.children;
 	for i = 1, #children do
@@ -54,23 +56,91 @@ function CHAT_MOD:Init(chat)
 			end
 		end
 	end
-	hook.Call("ChatModInitialize", self);
+	hook.Run("ChatModInitialize", self);
 end
 
 hook.Add("PlayerBindPress", "ObtainChat", function(p, bind, isdown)
-	if(bind == "messagemode") then
-		timer.Simple(0, function()
-			CHAT_MOD:Init(vgui.GetKeyboardFocus():GetParent():GetParent());
-		end);
+	if(isdown) then
+		if(bind == "messagemode" or bind == "messagemode2") then
+			timer.Simple(0.2, function()
+				if(not IsValid(vgui.GetKeyboardFocus())) then return; end
+				CHAT_MOD:Init(vgui.GetKeyboardFocus():GetParent():GetParent());
+				hook.Remove("PlayerBindPress", "ObtainChat");
+			end);
+		end
 	end
-	hook.Remove("PlayerBindPress", "ObtainChat");
 end);
 
 --[[
 	example:
 		
-		
+
+local old = {
+	--[[
+	[panel]	= {
+		w = 100;
+		h = 100;
+		x = 100;
+		y = 100;
+	};
+	]]--
+};
+
+local function setup(pnl)
+	local x, y = pnl:GetPos();
+	old[pnl] = old[pnl] or {
+		x = x;
+		y = y;
+		w = pnl:GetWide();
+		h = pnl:GetTall();
+	};
+end
+
+local width = ScrW() / 4 * 3;
 hook.Add("ChatModInitialize", "Example", function(chat)
 	chat:SetFont("TargetIDSmall");
+	local function background(self)
+		surface.SetDrawColor(0,100,0,100);	
+		surface.DrawRect(0,0,self:GetSize());
+	end
+	
+	local main = chat:Get(CHAT_MAIN);
+	local history = chat:Get(CHAT_HISTORY);
+	local input = chat:Get(CHAT_INPUT);
+	local input_main = chat:Get(CHAT_INPUT_LINE);
+	local filters = chat:Get(CHAT_FILTER_BUTTON);
+	
+	--[[ for restoration of chat ]]--
+	setup(main);
+	setup(history);
+	setup(input);
+	setup(input_main);
+	setup(filters);
+	
+	local wdif = width - main:GetWide();
+	
+	input.Paint = background;
+	history.Paint = background;
+	
+	local x,y = main:GetPos();
+	main:SetPos(ScrW() / 2 - width / 2, y);
+	local difference = width - main:GetWide();
+	main:SetWide(width);
+	
+	history:SetWide(history:GetWide() + wdif);
+	input_main:SetWide(input_main:GetWide() + wdif);
+	input:SetWide(input:GetWide() + wdif);
+	x,y = filters:GetPos();
+	filters:SetPos(filters:GetParent():GetWide() - filters:GetWide() - 2, y);
+	
+	local btn = vgui.Create("DButton", chat:Get(CHAT_MAIN));
+end);
+
+hook.Add("Shutdown", "Example", function()
+	for k,v in pairs(old) do
+		k:SetWide(v.w);
+		k:SetTall(v.h);
+		k:SetPos(v.x, v.y);
+	end
 end);
 ]]--
